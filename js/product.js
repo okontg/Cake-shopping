27fb870a2c6cb288d3adf1f54289967df8844163
okontg <thankgodgoodnews51@gmail.com>
2024-11-22 12:45:42 +0100

    Cake Website

---
+++ js/product.js
@@ -0,0 +1,55 @@
+export const product = [{
+  id: 'Add90-Hyi67qw',
+  firstName: 'Chocolate Cake',
+  secondName: 'Raspberry Apple Syrup',
+  image: '../image/chocolate-cake-raspberry-apples-syrup.jpg',
+  priceCents: 2000
+},{
+  id: 'Qert-tyoooDS',
+  firstName: 'Red Cake',
+  secondName: 'Red Velvet Heart',
+  image: '../image/red-velvet-cake-heart.jpg',
+  priceCents: 1500
+},{
+  id: 'YToof-90Aasdt',
+  firstName: 'Chocolate Cake',
+  secondName: 'Delicious cake',
+  image: '../image/chocolate-cake.jpg',
+  priceCents: 9900
+},{
+  id: 'AUr-9900gQ-GCa',
+  firstName: 'Chocolate Cake',
+  secondName: 'Cream Nuts',
+  image: '../image/chocolate-cake-with-cream-nuts.jpg',
+  priceCents: 7500
+},{
+  id: 'sdytO-tyA004rtt',
+  firstName: 'Sprinkes Strawberries',
+  secondName: 'Chocolate Strawberries',
+  image: '../image/chocolate-sprinkles-strawberries.jpg',
+  priceCents: 1400
+},{
+  id: 'utyghpo',
+  firstName: 'Cake',
+  secondName: 'Strawberries Cake',
+  image: '../image/strawberries-round-sweet.jpg',
+  priceCents: 1100
+},{
+  id: 'tyupo-Wsghi',
+  firstName: 'Strawberries Cake',
+  secondName: 'Chocolate Strawberries',
+  image: '../image/chocolate-with-strawberries.jpg',
+  priceCents: 3000
+},{
+  id: 'sdA004rtt',
+  firstName: 'Creaming',
+  secondName: 'Cream Syrup Cake',
+  image: '../image/cream-chocolate-syrup-plate.jpg',
+  priceCents: 1400
+},{
+  id: 'sdR-tt',
+  firstName: 'Cokie Cake',
+  secondName: 'Confectionery Cake',
+  image: '../image/sweet-cookie-confectionery.jpg',
+  priceCents: 1800
+}]
